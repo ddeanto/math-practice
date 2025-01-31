7b8909ef1f7e137cd41feb8fd4677d102a0f3117
ddeanto <darrengdeanto@gmail.com>
2025-01-31 16:12:34 -0500

    subtraction too

--- practice.py
+++ practice.py
@@ -1,24 +1,47 @@
 from random import randint, random, choice
+from tokenize import Funny
 
 
 def question():
 	while True:
-		r1 = randint(2, 15)
-		r2 = randint(5, 15)
-		if r1 == 10 or r2 == 10:
-			continue
+		add_or_subtract = "add" if randint(0, 1) else "subtract"
+		r1 = randint(1, 20)
+		r2 = randint(1, 9)
+		# if r1 == 10 or r2 == 10:
+		# 	continue
 
-		if random() < 0.5:
-			r1, r2 = r2, r1
-		
+		# if random() < 0.5:
+		# 	r1, r2 = r2, r1
+
+		if r2 < r1:
+			r2, r1 = r1, r2
+
 		while True:
-			answer = input(f"what's {r1} + {r2}: ")
-			if int(answer) == int(r1 + r2):
-				print("correct\n")
-				return
+			if add_or_subtract == "add":
+				answer = input(f"what's {r2} + {r1}: ")
+				if int(answer) == int(r2 + r1):
+					print("correct\n")
+					return
+				else:
+					print("incorrect. try again.")
 			else:
-				print("incorrect. try again.")
+				answer = input(f"what's {r2} - {r1}: ")
+				if int(answer) == int(r2 - r1):
+					print("correct\n")
+					return
+				else:
+					print("incorrect. try again.")
 			
-
-for n in range(20):
+for n in range(10):
 	question()
+
+
+"""
+* * * 
+* * *
+* * *
+* * *
+* * *
+"""
+
+
